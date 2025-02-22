d72e49007a479f3501b88c2bedc32897863dff0d
Ashutosh Jha <ashutoshj887@gmail.com>
2025-02-22 15:05:57 +0530

    feat: add logout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,19 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { SignOutButton } from "@clerk/nextjs";
 import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="flex justify-center items-center h-screen">Dashboard</div>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold text-primary">Dashboard</h1>
+      <SignOutButton>
+        <Button className="mt-6 bg-primary text-primary-foreground hover:bg-primary/80">
+          Logout
+        </Button>
+      </SignOutButton>
+    </div>
   );
 };
 
