58438e2bd8e97fe9da6784f3588c6afef6444c30
Ashutosh Jha <ashutoshj887@gmail.com>
2025-02-22 15:01:09 +0530

    fix: auth configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,9 @@
 import { Button } from "@/components/ui/button";
 import appConfig from "../config/appConfig";
 import { useRouter } from "next/navigation";
-import { useUser } from "@clerk/nextjs";
 
 export default function Home() {
   const router = useRouter();
-  const { isSignedIn } = useUser();
-
-  if (isSignedIn) {
-    router.push(appConfig.redirectAfterSignIn || "/dashboard");
-    return null;
-  }
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-background text-foreground px-4">
@@ -22,7 +15,7 @@ export default function Home() {
       </h2>
       <Button
         className="mt-6 bg-primary text-primary-foreground hover:bg-primary/80"
-        onClick={() => router.push("/sign-in")}
+        onClick={() => router.push(appConfig.appRoutes.signIn || "/sign-in")}
       >
         Get Started
       </Button>
--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,7 +1,12 @@
 const appConfig = {
   appName: "PitchGenie",
   appDescription: "AI that crafts the perfect pitch and follow-up!",
-  redirectAfterSignIn: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+  appRoutes: {
+    root: "/",
+    signIn: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    dashboard: "/dashboard",
+    afterSignIn: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+  },
 };
 
 export default appConfig;
