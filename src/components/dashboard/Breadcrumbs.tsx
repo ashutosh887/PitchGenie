37ba314647836dbb9a7b6cc27a177e3a72ee6371
Ashutosh Jha <ashutoshj887@gmail.com>
2025-02-23 00:08:14 +0530

    fix: breadcrumbs

--- src/components/dashboard/Breadcrumbs.tsx
+++ src/components/dashboard/Breadcrumbs.tsx
@@ -2,57 +2,68 @@
 
 import { usePathname } from "next/navigation";
 import { UserButton } from "@clerk/nextjs";
+import { ChevronRight, Bell } from "lucide-react";
 import {
   Breadcrumb,
-  BreadcrumbList,
   BreadcrumbItem,
   BreadcrumbLink,
+  BreadcrumbList,
   BreadcrumbPage,
 } from "@/components/ui/breadcrumb";
+import navConfig from "@/config/navConfig";
 
-// TODO: fix BreadCrumbs
+const getTitleFromPath = (path: string) => {
+  for (const section of navConfig) {
+    if (section.path === path) return section.title;
+    if (section.subSections) {
+      for (const subSection of section.subSections) {
+        if (subSection.path === path) return subSection.title;
+      }
+    }
+  }
+  return null;
+};
+
 const Breadcrumbs = () => {
   const pathname = usePathname();
   const paths = pathname.split("/").filter(Boolean);
 
   return (
-    <div className="p-3 bg-background dark:bg-background-dark flex justify-between items-center border-b border-[#27272A]">
+    <div className="p-4 bg-background dark:bg-background-dark flex justify-between items-center border-b border-[#27272A]">
       <Breadcrumb>
         <BreadcrumbList className="flex items-center space-x-2">
-          {pathname === "/dashboard/settings" ? (
-            // Show only "Settings" when on the Settings page
-            <BreadcrumbItem>
-              <BreadcrumbPage className="capitalize text-foreground dark:text-foreground-dark">
-                Settings
-              </BreadcrumbPage>
-            </BreadcrumbItem>
-          ) : (
-            // Default breadcrumb logic
-            paths.map((path, index) => (
-              <BreadcrumbItem key={index}>
+          {paths.map((_, index) => {
+            const fullPath = `/${paths.slice(0, index + 1).join("/")}`;
+            const title =
+              getTitleFromPath(fullPath) || paths[index].replace("-", " ");
+
+            return (
+              <BreadcrumbItem key={index} className="flex items-center">
                 {index !== paths.length - 1 ? (
-                  <BreadcrumbLink
-                    href={`/${paths.slice(0, index + 1).join("/")}`}
-                    className="capitalize text-foreground dark:text-foreground-dark"
-                  >
-                    {path.replace("-", " ")}
-                  </BreadcrumbLink>
+                  <>
+                    <BreadcrumbLink
+                      href={fullPath}
+                      className="hover:text-primary transition-colors"
+                    >
+                      {title}
+                    </BreadcrumbLink>
+                    <ChevronRight className="w-4 h-4 text-muted-foreground dark:text-muted-foreground-dark" />
+                  </>
                 ) : (
-                  <BreadcrumbPage className="capitalize text-foreground dark:text-foreground-dark">
-                    {path.replace("-", " ")}
+                  <BreadcrumbPage className="font-semibold">
+                    {title}
                   </BreadcrumbPage>
                 )}
-                {index !== paths.length - 1 && (
-                  <span className="text-muted-foreground dark:text-muted-foreground-dark">
-                    /
-                  </span>
-                )}
               </BreadcrumbItem>
-            ))
-          )}
+            );
+          })}
         </BreadcrumbList>
       </Breadcrumb>
-      <div className="flex items-center space-x-1">
+
+      <div className="flex items-center space-x-2">
+        <Bell />
+        <ChevronRight className="w-1 h-1 text-muted-foreground dark:text-muted-foreground-dark" />
+
         <UserButton />
       </div>
     </div>
