2ebab864b0ee6828c256e51f94e8718682994331
Ashutosh Jha <ashutoshj887@gmail.com>
2025-02-23 11:23:30 +0530

    remove: fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+// import { Geist } from "next/font/google";
 import "./globals.css";
 import appConfig from "../config/appConfig";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { ToastProviderWrapper } from "@/components/ui/use-toast";
 
-const geist = Geist({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
+// const geist = Geist({
+//   subsets: ["latin"],
+//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+// });
 
 export const metadata: Metadata = {
   title: appConfig.appName,
@@ -21,7 +21,8 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`antialiased ${geist.className}`}>
+      {/* <body className={`antialiased ${geist.className}`}> */}
+      <body className={`antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="dark">
           <ClerkProvider>
             <ToastProviderWrapper>{children}</ToastProviderWrapper>
